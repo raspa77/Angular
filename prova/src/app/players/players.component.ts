1914ed5c40ca649a619d6feb0af94dcf58bc77d2
raspa77 <l.raspatelli@gmail.com>
2018-12-30 15:11:29 +0100

    Primo commit

---
+++ prova/src/app/players/players.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+    selector : 'app-players',
+    template: '<h2>Players</h2>'
+})
+export class PlayersComponent {
+}
