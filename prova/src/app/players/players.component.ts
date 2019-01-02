16d52866f66300d7b97eb783b6101550f0eb284b
raspa77 <l.raspatelli@gmail.com>
2019-01-02 15:17:00 +0100

    Aggiunte righe ngfor alla tabella

--- prova/src/app/players/players.component.ts
+++ prova/src/app/players/players.component.ts
@@ -7,4 +7,22 @@ import { template } from '@angular/core/src/render3';
     styleUrls: ['./players.component.css']
 })
 export class PlayersComponent {
+    title = 'Players';
+    players = [
+        {
+            name: 'Master',
+            pg: 'Master',
+            lev: 99
+        },
+        {
+            name: 'Raspa',
+            pg: 'Snatk',
+            lev: 13
+        },
+        {
+            name: 'Ciappy',
+            pg: 'Cornovald',
+            lev: 15
+        }
+    ];
 }
