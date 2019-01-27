c80a76b39ca57ddb937734af0b7db9874d136dbe
raspa77 <l.raspatelli@gmail.com>
2019-01-27 13:55:01 +0100

    aggiunto player

--- prova/src/app/app.module.ts
+++ prova/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
 import { PlayerService } from './players/player.service';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayersComponent
+    PlayersComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
